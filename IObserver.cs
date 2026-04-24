94eeabea33f4d748ab910b80ce0c55445f5c986f
anaclaralopeslouzada <ana.louzada@unifesp.br>
2026-04-24 14:46:42 -0300

    Create IObserver.cs

---
+++ IObserver.cs
@@ -0,0 +1,11 @@
+namespace Amazonia2
+{
+    // Esta interface define o contrato do CALLBACK.
+    // Eu não digo para a PCD qual universidade ela deve chamar, eu digo apenas que ela deve chamar um método chamado "Atualizar".
+    public interface IObserver
+    {
+        // Este método funciona como um CALLBACK. 
+        // É uma função que a universidade "deixa" com a PCD para ser chamada de volta quando algo acontecer.
+        void Atualizar(string nomePCD, double temp, double umidade);
+    }
+}
